9f8aa6ff53c6414a57f7b0637c50d8fe641ab84a
aramtd <aramtd@gmail.com>
2017-09-11 12:53:52 -0700

    Add files via upload

---
+++ 02unidad/exec.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <studio.h>
+
+main(){
+	pid_t pid;
+	pid = fork();
+	if(pid<0){
+		fprintf(stderr, "Fallo en fork()");
+		exit(-1);
+	}
+	else if(pid==0)
+		execlp("/bin/ls", "ls", NULL);
+	else{
+		wait(NULL);
+		printf("Proceso hijo concluido");
+		exit(0);
+	}
+}
---
+++ 02unidad/fork2.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <studio.h>
+
+main(){
+	int return_value;
+	printf("Creando procesos con fork\n");
+	return_value=fork();
+	if(return_value==0)
+		printf("El pid del proceso hijo es %d\n", getpid());
+	else
+		printf("El pid del proceso padre es %d\n", getpid());
+}
